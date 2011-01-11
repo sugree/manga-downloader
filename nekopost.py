f2a3585a1cec864b118d1227360c839d8af87281
sugree <sugree@gmail.com>
2011-01-11 22:56:44 +0700

    added category changed url

--- nekopost.py
+++ nekopost.py
@@ -7,19 +7,19 @@ from manga import Manga, App, urlopen
 
 class NekoPost(Manga):
 
-    SERIES_URL = '%(baseurl)s/content/%(series)s'
-    CHAPTER_URL = '%(baseurl)s/content/%(series)s/%(chapter)03d'
-    PAGE_URL = '%(baseurl)s/content/%(series)s/%(chapter)03d'
+    SERIES_URL = '%(baseurl)s/project/%(category)s/%(series)s'
+    CHAPTER_URL = '%(baseurl)s/project/%(category)s/%(series)s/%(chapter)03d'
+    PAGE_URL = '%(baseurl)s/project/%(category)s/%(series)s/%(chapter)03d'
 
-    CHAPTER_CRE = re.compile(r'content/[^/]+/(\d+)')
-    PAGE_CRE = re.compile(r'document\.img_content\.src = "([^"]+)" \+ dropdown_value;')
+    CHAPTER_CRE = re.compile(r'project/[^/]+/[^/]+/(\d+)')
+    PAGE_CRE = re.compile(r'var file_path = "([^"]+)";')
 
     def __init__(self):
         Manga.__init__(self, 'http://www.nekopost.net')
 
     def _list_chapters(self, doc):
         chapters = []
-        for n in doc.xpath("//ul[@class='chapter-list']/li[position()>1]/div[@class='service']"):
+        for n in doc.xpath("//ul[@class='list_chapter_ul']/li[position()>1]/div[@class='list_chapter_read']"):
             u = n.xpath("a[text()='Read']")[0].attrib['href']
             m = self.CHAPTER_CRE.match(u)
             chapters.append({'chapter': int(m.group(1))})
@@ -29,7 +29,7 @@ class NekoPost(Manga):
         pages = doc.xpath("//select[@id='dropdown_page']/option")
         self.page_map = dict([(int(i.text), i.attrib['value']) for i in pages])
         pages = [int(i.text) for i in pages]
-        m = self.PAGE_CRE.search(doc.xpath("//script[1]")[0].text)
+        m = self.PAGE_CRE.search(doc.xpath("//script")[1].text)
         self.page_url = m.group(1)
         return pages
 
@@ -45,7 +45,14 @@ class NekoPost(Manga):
 class NekoPostApp(App):
     def __init__(self):
         App.__init__(self)
+        if self.options.category:
+            self.data.update({'category': self.options.category})
         self.manga = NekoPost()
+
+    def _parse_args(self, parser):
+        App._parse_args(self, parser)
+        parser.add_option('--category', dest='category', default='Manga',
+                          help='Category e.g. manga, doujin, novel')
 
 if __name__ == '__main__':
     app = NekoPostApp()
