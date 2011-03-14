b0ec769ea61865e1e176dcc64f40349e10c6d38d
sugree <sugree@gmail.com>
2011-03-14 14:57:43 +0700

    added stoptazmo

---
+++ stoptazmo.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+import re
+
+from manga import Manga, App, urlretrieve
+
+class StopTazmo(Manga):
+
+    SERIES_URL = '%(baseurl)s/manga-series/%(series)s/'
+
+    CHAPTER_PATTERN = '%(series)s-%(chapter)s.cbz'
+
+    CHAPTER_CRE = re.compile(r'file_name=(?P<chapter_id>.*_(?P<chapter>[^.]+)\.zip)$')
+
+    def __init__(self):
+        Manga.__init__(self, 'http://stoptazmo.com')
+
+    def _list_chapters(self, doc):
+        chapters = []
+        for n in doc.xpath("//div[@id='content']/table/tbody/tr[position()>1]"):
+            u = n.xpath("td[2]/a")[0].attrib['href']
+            m = self.CHAPTER_CRE.search(u)
+            chapters.append({'chapter': m.group('chapter'),
+                             'chapter_id': m.group('chapter_id'),
+                             'chapter_url': u})
+        return chapters
+
+    def download_chapter(self, data):
+        print data
+        filename = self.get_chapter_filename(data)
+        url = data['chapter_url']
+        content = urlretrieve(url, headers=self.http_headers)
+        fo = open(filename, 'wb')
+        fo.write(content)
+        fo.close()
+
+    def zip_chapter(self, pages, data):
+        pass
+
+    def remove_chapter(self, pages, data):
+        pass
+
+class StopTazmoApp(App):
+    def __init__(self):
+        App.__init__(self, chapter_func=str)
+        self.manga = StopTazmo()
+
+    def _parse_args(self, parser):
+        App._parse_args(self, parser)
+
+if __name__ == '__main__':
+    app = StopTazmoApp()
+    app.run()
